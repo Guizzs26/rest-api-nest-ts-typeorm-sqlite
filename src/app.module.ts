9623abdca738bf956b0cd8beb31cd56df081772f
Guilherme <guiteixeira1926@gmail.com>
2024-02-28 15:41:22 -0400

    feat: add migration role table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { RolesModule } from '@roles/roles.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateRolesTable1709148513121 } from '@shared/typeorm/migrations/1709148513121-CreateRolesTable';
+import { Role } from '@roles/entities/role.entity';
 @Module({
-  imports: [RolesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './db.sqlite',
+      entities: [Role],
+      migrations: [CreateRolesTable1709148513121],
+      synchronize: true,
+      migrationsRun: true,
+    }),
+    RolesModule,
+  ],
   controllers: [],
   providers: [],
 })
