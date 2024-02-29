cfbb4170ae83cc28b3cec2396461f151e2f39a0a
Guilherme <guiteixeira1926@gmail.com>
2024-02-29 06:40:49 -0400

    refactoring: Refactoring role entity with typeorm decorators

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,10 +1,20 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 
+@Entity('roles')
 export class Role {
+  @PrimaryGeneratedColumn()
   id?: string;
 
+  @Column()
   name: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
