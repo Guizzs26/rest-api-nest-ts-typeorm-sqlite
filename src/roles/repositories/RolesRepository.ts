eebac33874ba505b86dc67b681025d55ced0ce86
Guilherme <guiteixeira1926@gmail.com>
2024-02-29 07:02:45 -0400

    refactoring: Roles repository refactoring

--- src/roles/repositories/RolesRepository.ts
+++ src/roles/repositories/RolesRepository.ts
@@ -1,37 +1,70 @@
 import { Injectable } from '@nestjs/common';
-import { Role } from '@roles/entities/role.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateRoleDTO } from '@roles/DTO/createRoleDTO';
+import { Role } from '@roles/entities/role.entity';
+import { skip } from 'node:test';
+import { Repository } from 'typeorm';
 
-const roles: Role[] = [];
+export type PaginateParams = {
+  page: number;
+  skip: number;
+  take: number;
+};
+
+export type RolesPaginateProperties = {
+  per_page: number;
+  total: number;
+  current_page: number;
+  data: Role[];
+};
 
 @Injectable()
 export class RolesRepository {
-  private roles: Role[];
+  constructor(
+    @InjectRepository(Role)
+    private rolesRepository: Repository<Role>,
+  ) {}
 
-  constructor() {
-    this.roles = [];
+  async create({ name }: CreateRoleDTO): Promise<Role> {
+    const role = this.rolesRepository.create({ name });
+
+    return this.rolesRepository.save(role);
   }
 
-  create({ name }: CreateRoleDTO): Role {
-    const role = new Role();
-
-    Object.assign(role, {
-      name,
-      created_at: new Date(),
-    });
-
-    roles.push(role);
-
-    return role;
+  async save(role: Role): Promise<Role> {
+    return this.rolesRepository.save(role);
   }
 
-  findAll(): Role[] {
-    return this.roles;
+  async delete(role: Role): Promise<void> {
+    await this.rolesRepository.remove(role);
   }
 
-  findByName(name: string): Role | undefined {
-    const role = this.roles.find((role) => role.name === name);
+  async findAll({
+    page,
+    skip,
+    take,
+  }: PaginateParams): Promise<RolesPaginateProperties> {
+    const [roles, count] = await this.rolesRepository
+      .createQueryBuilder()
+      .skip(skip)
+      .take(take)
+      .getManyAndCount();
 
-    return role;
+    const result = {
+      per_page: take,
+      total: count,
+      current_page: page,
+      data: roles,
+    };
+
+    return result;
+  }
+
+  async findById(id: string): Promise<Role | null> {
+    return this.rolesRepository.findOneBy({ id });
+  }
+
+  async findByName(name: string): Promise<Role | null> {
+    return this.rolesRepository.findOneBy({ name });
   }
 }
